3d14f4f14f0ac5317ae1c927cf265aa4b8cf427e
Lokesh Singana <lokeshsingana9@gmail.com>
2021-12-01 17:43:00 +0530

    Commit 1

---
+++ pattern.py
@@ -0,0 +1,13 @@
+n=7
+for j in range(1,n+1):
+    k=1
+    for i in range(1,n+1):
+        k+=1
+        if i<=j:
+            print(k,end=' ')
+        else:
+            print(' ',end=' ')
+        
+    print( )
+           
+        
